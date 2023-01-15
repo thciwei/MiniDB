aae6e1c66924cb9f3a24502d5e787e490c66a832
thciwei <65482477+thciwei@users.noreply.github.com>
2023-01-15 11:17:09 +0800

    feat:完成了TableManager

---
+++ src/main/java/com/thciwei/minidb/backend/tbm/BeginRes.java
@@ -0,0 +1,11 @@
+package com.thciwei.minidb.backend.tbm;
+
+/**
+ * @author thciwei
+ * @email qiaosong.wang@foxmail.com
+ * @desc
+ */
+public class BeginRes {
+    public long xid;
+    public byte[] result;
+}
